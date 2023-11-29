f8e642435f027c68894c99efbb73b6da98edd2cc
Colin16655 <96989306+Colin16655@users.noreply.github.com>
2023-11-29 17:57:31 +0100

    Add files via upload

--- Lab2/Pendubot_Simulator_2023/Pendubot_Simulator_2021/pendubot_reg.m
+++ Lab2/Pendubot_Simulator_2023/Pendubot_Simulator_2021/pendubot_reg.m
@@ -13,6 +13,56 @@ function u=pendubot_reg(alpha1r,x)
 %OUTPUT:
 % u       : control signal (motor torque) to be applied [Nm]
 
+syms a1 a2 da1 da2 t;
 
+% Define the constants
+p1 = 0.0148;
+p2 = 0.0051;
+p3 = 0.0046;
+p4 = 0.1003;
+p5 = 0.0303;
+g  = 9.81;
+k = 3.9621/8;
 
-u=3*((alpha1r-pi)-x(1))-0.3*x(3);
+% Define matrices M, Vm, G
+M  = [p1+p2+2*p3*cos(a2-a1), p2+p3*cos(a2-a1); p2+p3*cos(a2-a1), p2];
+Vmda = p3*sin(a2-a1) * [da1^2-da1*da2-da2^2; da1^2];
+G  = [p4*g*sin(a1); p5*g*sin(a2)];
+
+% Define the state vector and input
+x_notice = [a1; a2; da1; da2];
+
+% Define the nonlinear dynamics f(x) and g(x)
+f1 = da1;
+f2 = da2;
+f34 = M\(-Vmda-G);
+f3 = f34(1);
+f4 = f34(2);
+
+g34 = mldivide(M, [k; 0]);
+g3 = g34(1);
+g4 = g34(2);
+
+f = [f1; f2; f3; f4];
+g = [0;0;g3;g4];
+
+% Compute the Jacobian matrix A
+A = jacobian(f, x_notice) + jacobian(g, x_notice)*t;
+
+% Compute the Jacobian matrix B
+B = g;
+
+% Set the equilibrium point
+x_e = [4*pi/5; pi; 0; 0];
+t_e = 10; % change
+
+% Substitute the equilibrium point into the matrices A, B, and C
+A_e = double(subs(A, {a1, a2, da1, da2, t}, {x_e(1), x_e(2), x_e(3), x_e(4), t_e}));
+B_e = double(subs(B, {a1, a2, da1, da2, t}, {x_e(1), x_e(2), x_e(3), x_e(4), t_e}));
+
+% Control Law
+Q = [1, 0, 0, 0; 0, 1, 0, 0; 0, 0, 1, 0; 0, 0, 0, 1];
+R = 1;
+K = lqr(A_e, B_e, Q, R);
+corr_x_measured = [x(1)+pi; x(2)+pi; x(3); x(4)];
+u=-K*corr_x_measured;
