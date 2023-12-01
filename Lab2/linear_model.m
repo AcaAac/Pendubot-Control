2095afe804b9df73735daaed57d314ed11fc660c
Colin16655 <96989306+Colin16655@users.noreply.github.com>
2023-12-01 13:04:12 +0100

    Add files via upload

--- Lab2/linear_model.m
+++ Lab2/linear_model.m
@@ -15,7 +15,7 @@ Vmda = p3*sin(a2-a1) * [da1^2-da1*da2-da2^2; da1^2];
 G  = [p4*g*sin(a1); p5*g*sin(a2)];
 
 % Define the state vector and input
-x = [a1; a2; da1; da2];
+x_notice = [a1; a2; da1; da2];
 
 % Define the nonlinear dynamics f(x) and g(x)
 f1 = da1;
@@ -24,7 +24,7 @@ f34 = M\(-Vmda-G);
 f3 = f34(1);
 f4 = f34(2);
 
-g34 = mldivide(M, [k; 0]);
+g34 = M\([k; 0]);
 g3 = g34(1);
 g4 = g34(2);
 
@@ -32,44 +32,41 @@ f = [f1; f2; f3; f4];
 g = [0;0;g3;g4];
 
 % Compute the Jacobian matrix A
-A = jacobian(f, x) + jacobian(g, x)*t;
+A = jacobian(f, x_notice) + jacobian(g, x_notice)*t;
 
 % Compute the Jacobian matrix B
 B = g;
 
-% Define the output vector y (in this case, all state variables are outputs)
-C = eye(length(x));
-
 % Set the equilibrium point
 x_e = [4*pi/5; pi; 0; 0];
-t_e = 10; % change
+t_e = sin(x_e(1))*p4;
 
 % Substitute the equilibrium point into the matrices A, B, and C
 A_e = double(subs(A, {a1, a2, da1, da2, t}, {x_e(1), x_e(2), x_e(3), x_e(4), t_e}));
 B_e = double(subs(B, {a1, a2, da1, da2, t}, {x_e(1), x_e(2), x_e(3), x_e(4), t_e}));
-C_e = C;
-D_e = [0;0;0;0];
 
 % Construct the linear state-space representation
-sys = ss(A_e, B_e, C_e, D_e);
+sys = ss(A_e, B_e, eye(4), 0);
 
-% Display the matrices
-disp('A_e:');
-disp(A_e);
+% Discretize
+% Specify the time step for discretization
+dt = 0.004; % You can adjust this value as needed
 
-disp('B_e:');
-disp(B_e);
+% Discretize the system using c2d
+sys_d = c2d(sys, dt, 'zoh');
 
-disp('C_e:');
-disp(C_e);
+% Display the discretized system matrices
+disp('Discretized A_d and B_d:');
+disp(sys_d.A);
+disp(sys_d.B);
 
 % Control Law
-Q = [1, 0, 0, 0; 0, 1, 0, 0; 0, 0, 1, 0; 0, 0, 0, 1];
+Q = [10, 0, 0, 0; 0, 10, 0, 0; 0, 0, 1, 0; 0, 0, 0, 1];
 R = 1;
-K = lqr(A_e, B_e, Q, R);
+K = dlqr(sys_d.A, sys_d.B, Q, R);
 
 % Displaying
-disp('Linear State-Space System:');
-disp(sys);
+%disp('Linear State-Space System:');
+%disp(sys);
 disp('LQR, K :');
 disp(K);
